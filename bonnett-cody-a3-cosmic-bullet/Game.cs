0ea1a5f6d5da5b2582d48645c7107642d8a537d5
c-bonn3tt <cody.bonnett@mohawkcollege.ca>
2025-11-06 00:02:07 -0500

    initial stuff

--- bonnett-cody-a3-cosmic-bullet/Game.cs
+++ bonnett-cody-a3-cosmic-bullet/Game.cs
@@ -18,6 +18,9 @@ namespace MohawkGame2D
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Cosmic Bullet");
+            Window.SetSize(800, 600);
+
 
         }
 
@@ -26,6 +29,7 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.Black);
 
         }
     }
